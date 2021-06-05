ff7d064b67ee4b86ca10694a52af1b73b60cfa2a
timofeya123 <timofeya123@inbox.ru>
2021-06-05 15:23:31 +0600

    Tetris_board_comment

--- src/com/AWT/Board.java
+++ src/com/AWT/Board.java
@@ -13,32 +13,33 @@ import javax.swing.Timer;
 
 import com.AWT.Shape.Tetrominoes;
 
-public class Board extends JPanel implements ActionListener {
+public class Board extends JPanel implements ActionListener { // класс для игры, где будут происходить действия / в виде панели
 
-    private final int BoardWidth = 10;
+    private final int BoardWidth = 10; // размеры
     private final int BoardHeight = 22;
 
-    private Timer timer;
-    private boolean isFallingFinished = false;
+    private Timer timer; // таймер
+    private boolean isFallingFinished = false; // статусы / isFallingFinished Переменная определяет, если форма Tetris закончил падать , и затем мы должны создать новую форму.
     private boolean isStarted = false;
     private boolean isPaused = false;
-    private int numLinesRemoved = 0;
+    private int numLinesRemoved = 0; // В numLinesRemovedпод считывает количество строк, мы удалили до сих пор.
     private int curX = 0;
-    private int curY = 0;
+    private int curY = 0; // curX И curY переменные определяют фактическое положение падающей формы Тетриса.
     private JLabel statusbar;
     private Shape curPiece;
     private Shape.Tetrominoes[] board;
 
-    public Board(Tetris parent) {
-
-        initBoard(parent);
+    public Board(Tetris parent) { // конструктор
+        initBoard(parent); // инициализация
     }
 
-    private void initBoard(Tetris parent) {
+    // ------------------------------------------------------------------------------ инициализация
 
-        setFocusable(true);
-        curPiece = new Shape();
-        timer = new Timer(400, this);
+    private void initBoard(Tetris parent) { // инициализация
+
+        setFocusable(true); // Мы должны явно вызвать setFocusable() метод. Теперь на плате есть ввод с клавиатуры.
+        curPiece = new Shape(); // фигуры
+        timer = new Timer(400, this); // запускаем таймер
         timer.start();
 
         statusbar =  parent.getStatusBar();
@@ -47,15 +48,15 @@ public class Board extends JPanel implements ActionListener {
         clearBoard();
     }
 
-    @Override
+    @Override // В нашем случае таймер вызывает actionPerformed()метод каждые 400 мсек.
     public void actionPerformed(ActionEvent e) {
 
-        if (isFallingFinished) {
-
+        if (isFallingFinished) { // В actionPerformed()методе проверяет , если падение закончилось.
+                                    // Если это так, создается новая фигура.
             isFallingFinished = false;
             newPiece();
         } else {
-
+            // В противном случае падающий кусок тетриса переходит на одну строчку вниз.
             oneLineDown();
         }
     }
@@ -65,7 +66,7 @@ public class Board extends JPanel implements ActionListener {
     private Tetrominoes shapeAt(int x, int y) { return board[(y * BoardWidth) + x]; }
 
 
-    public void start()  {
+    public void start()  { // --------------------------------------------------- запуск
 
         if (isPaused)
             return;
@@ -79,7 +80,7 @@ public class Board extends JPanel implements ActionListener {
         timer.start();
     }
 
-    private void pause()  {
+    private void pause()  { // ------------------------------------------------------ пауза
 
         if (!isStarted)
             return;
@@ -99,14 +100,16 @@ public class Board extends JPanel implements ActionListener {
         repaint();
     }
 
+    // ---------------------------------------------------------Внутри метода doDrawing () мы рисуем все объекты на доске. Картина состоит из двух этапов.
+
     private void doDrawing(Graphics g) {
 
         Dimension size = getSize();
         int boardTop = (int) size.getHeight() - BoardHeight * squareHeight();
 
-        for (int i = 0; i < BoardHeight; ++i) {
+        for (int i = 0; i < BoardHeight; ++i) { // На первом этапе мы рисуем все формы или остатки фигур, которые были опущены на нижнюю часть доски.
 
-            for (int j = 0; j < BoardWidth; ++j) {
+            for (int j = 0; j < BoardWidth; ++j) {// Все квадраты запоминаются в массиве доски. Мы обращаемся к нему с помощью shapeAt()метода.
 
                 Tetrominoes shape = shapeAt(j, BoardHeight - i - 1);
 
@@ -116,7 +119,7 @@ public class Board extends JPanel implements ActionListener {
             }
         }
 
-        if (curPiece.getShape() != Tetrominoes.NoShape) {
+        if (curPiece.getShape() != Tetrominoes.NoShape) { // На втором этапе мы рисуем собственно падающую деталь.
 
             for (int i = 0; i < 4; ++i) {
 
@@ -130,17 +133,17 @@ public class Board extends JPanel implements ActionListener {
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-
+    public void paintComponent(Graphics g) { // отрисовка компонентов
         super.paintComponent(g);
         doDrawing(g);
     }
 
+    // --------------------------------------------------------------------------- Если мы нажмем клавишу пробела, кусок опускается вниз.
     private void dropDown() {
 
         int newY = curY;
 
-        while (newY > 0) {
+        while (newY > 0) { // Мы просто пытаемся опустить кусок на одну линию вниз, пока он не достигнет низа или вершины другого выпавшего фрагмента Тетриса.
 
             if (!tryMove(curPiece, curX, newY - 1))
                 break;
@@ -156,36 +159,38 @@ public class Board extends JPanel implements ActionListener {
             pieceDropped();
     }
 
-
+    // ----------------------------------------------------------- clearBoard() Метод заполняет доску с пустым NoShapes. Позже это используется при обнаружении столкновений.
     private void clearBoard() {
 
         for (int i = 0; i < BoardHeight * BoardWidth; ++i)
             board[i] = Tetrominoes.NoShape;
     }
 
+    // ---------------------------------------------------------------- pieceDropped() Метод помещает падающий кусок в доски массива.
     private void pieceDropped() {
-
+        //Еще раз, доска содержит все квадраты фигур и остатки тех фигур, которые закончили падать.
         for (int i = 0; i < 4; ++i) {
 
             int x = curX + curPiece.x(i);
             int y = curY - curPiece.y(i);
             board[(y * BoardWidth) + x] = curPiece.getShape();
         }
-
-        removeFullLines();
+                            //Когда фигура закончила падать, пора проверить, сможем ли мы убрать несколько линий с доски.
+        removeFullLines(); //Это работа removeFullLines() метода. Затем создаем новый кусок. Точнее, мы пытаемся создать новое произведение.
 
         if (!isFallingFinished)
             newPiece();
     }
 
+    //---------------------------------------------------------------------- newPiece() Метод создает новый Тетрис кусок.
     private void newPiece()  {
 
-        curPiece.setRandomShape();
-        curX = BoardWidth / 2 + 1;
+        curPiece.setRandomShape(); // Изделие приобретает новую случайную форму.
+        curX = BoardWidth / 2 + 1; // Затем мы вычисляем начальное значение curX и curY значения.
         curY = BoardHeight - 1 + curPiece.minY();
 
-        if (!tryMove(curPiece, curX, curY)) {
-
+        if (!tryMove(curPiece, curX, curY)) { // Если мы не можем перейти на исходные позиции, игра окончена. Мы на вершине.
+            // Таймер остановлен. Мы помещаем игру поверх строки в строку состояния.
             curPiece.setShape(Tetrominoes.NoShape);
             timer.stop();
             isStarted = false;
@@ -193,9 +198,10 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    // ---------------------------------------------------------------------------- tryMove() Метод пытается переместить часть Тетрис.
     private boolean tryMove(Shape newPiece, int newX, int newY) {
 
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 4; ++i) { // Метод возвращает false, если он достиг границ доски или находится рядом с уже выпавшими фишками тетриса.
 
             int x = newX + newPiece.x(i);
             int y = newY - newPiece.y(i);
@@ -216,11 +222,12 @@ public class Board extends JPanel implements ActionListener {
         return true;
     }
 
+    // -------------------------------------------- Внутри removeFullLines()метода мы проверяем, есть ли среди всех строк на доске какая-либо полная строка .
     private void removeFullLines() {
 
         int numFullLines = 0;
 
-        for (int i = BoardHeight - 1; i >= 0; --i) {
+        for (int i = BoardHeight - 1; i >= 0; --i) { // Если есть хотя бы одна полная строка, она удаляется
             boolean lineIsFull = true;
 
             for (int j = 0; j < BoardWidth; ++j) {
@@ -230,7 +237,7 @@ public class Board extends JPanel implements ActionListener {
                 }
             }
 
-            if (lineIsFull) {
+            if (lineIsFull) {//Найдя полную строку, увеличиваем счетчик
                 ++numFullLines;
                 for (int k = i; k < BoardHeight - 1; ++k) {
                     for (int j = 0; j < BoardWidth; ++j)
@@ -239,7 +246,7 @@ public class Board extends JPanel implements ActionListener {
             }
         }
 
-        if (numFullLines > 0) {
+        if (numFullLines > 0) { // Перемещаем все строки над полным рядом на одну строку вниз. Таким образом мы уничтожаем всю линию.
 
             numLinesRemoved += numFullLines;
             statusbar.setText(String.valueOf(numLinesRemoved));
@@ -249,6 +256,8 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+    // ---------------------------------------------------------------Каждая фигура тетриса состоит из четырех квадратов.
+    // ---------------------------------------------------------------Каждый из квадратов нарисован drawSquare()методом. Фишки тетриса имеют разные цвета.
     private void drawSquare(Graphics g, int x, int y, Tetrominoes shape)  {
 
         Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102),
@@ -256,6 +265,8 @@ public class Board extends JPanel implements ActionListener {
                 new Color(204, 204, 102), new Color(204, 102, 204),
                 new Color(102, 204, 204), new Color(218, 170, 0)
         };
+        // Левая и верхняя стороны квадрата отображаются более ярким цветом.
+        // Точно так же нижняя и правая стороны нарисованы более темными цветами. Это для имитации 3D-кромки.
 
         Color color = colors[shape.ordinal()];
 
@@ -274,6 +285,8 @@ public class Board extends JPanel implements ActionListener {
 
     }
 
+    // --------------------------------------------------- Управляем игрой с клавиатуры. Механизм управления реализован с помощью KeyAdapter.
+    // --------------------------------------------------- Это внутренний класс, который переопределяет keyPressed()метод.
     class TAdapter extends KeyAdapter {
 
         @Override
@@ -295,7 +308,7 @@ public class Board extends JPanel implements ActionListener {
 
             switch (keycode) {
 
-                case KeyEvent.VK_LEFT:
+                case KeyEvent.VK_LEFT: // Если мы нажмем клавишу со стрелкой влево, мы попытаемся переместить падающую фигуру на один квадрат влево.
                     tryMove(curPiece, curX - 1, curY);
                     break;
 
